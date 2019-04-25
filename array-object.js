8dca7ddc4496d7acd0cd4537de2d0b763f55da45
Trang Luu <tluu9@mail.greenriver.edu>
2019-04-25 01:24:15 -0400

    Added an js file

---
+++ array-object.js
@@ -0,0 +1,34 @@
+/**
+ * Trang Luu
+ * 24 APR 2019
+ * Javascript Practice # 4
+ */
+
+
+//Create an array
+var input =[-1 ,5 ,"cat" ,false ,10.2 ,true ,"dog"];
+
+var result;
+
+
+//Create an object
+function array(input) {
+    result = {
+        strings: [],
+        numbers: [],
+        booleans: []
+    };
+
+
+    //Passing the values from array and sort them out by type
+    for (var i = 0; i < input.length; i++) {
+        if (typeof input[i] == "number") {
+            document.write(" numbers: [" +input[i]+ " ]" + "<br>");
+        } else if (typeof input[i] == "boolean") {
+            document.write("booleans : [" +input[i]+ " ]" + "<br>");
+        } else {
+            document.write("string : [" +input[i]+ " ]" + "<br>");
+        }
+    }
+}
+array(input);
